3729c8a47ddf358c5966a3161ff0859bbe066d32
Ben-Cousins12345 <b8007703@my.shu.ac.uk>
2020-01-31 15:26:38 +0000

    all week one tasks completed

---
+++ enrolSystem/grails-app/domain/enrolsystem/Course.groovy
@@ -0,0 +1,30 @@
+package enrolsystem
+
+class Course {
+
+	String department;
+	String courseTitle;
+	String courseLeader;
+	String courseCode;
+
+	Date startDate;
+	Date endDate;
+	String description;
+	int numberOfStudents;
+	double tuitionFees;
+	String studyMode;
+    static constraints = {
+
+	department nullable: false, blank: false
+	courseTitle nullable: false, blank: false
+	courseLeader nullable: false, blank: false
+	courseCode nullable: false, blank: false
+
+	startDate nullable: false, blank: false
+	endDate nullable: false, blank: false
+	description nullable: false, blank: false
+	numberOfStudents nullable: false, blank: false, minSize: 20, maxSize: 60
+	tuitionFees nullable: false, blank: false
+	studyMode nullable: false, blank: false
+    }
+}
